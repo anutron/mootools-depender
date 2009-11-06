f087c71d790b49cebe5cfa6d60bca86c82f0940e
Aaron Newton <git@clientcide.com>
2009-11-05 22:49:59 -0800

    adding element delegation to the requirements for the ui.

--- php/index.php
+++ php/index.php
@@ -9,7 +9,7 @@ $depender = New Depender;
 		<title>Depender - A MooTools Dependency Builder</title>
 		<link rel="stylesheet" href="../styles/reset.css" type="text/css" media="screen" title="no title" charset="utf-8">
 		<link rel="stylesheet" href="../styles/depender.css" type="text/css" media="screen" title="no title" charset="utf-8">
-		<script src="build.php?requireLibs=mootools-core&require=Fx.Reveal,URI,Element.Position,String.QueryString"></script>
+		<script src="build.php?requireLibs=mootools-core&require=Fx.Reveal,URI,Element.Position,String.QueryString,Element.Delegation"></script>
 		<script src="interface.js" type="text/javascript" charset="utf-8"></script>
 	</head>
 	<body>
