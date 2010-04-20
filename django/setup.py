8f0cc65dfc34a8cc412339b5f220f5a125209c97
Philip Zeyliger <philip@cloudera.com>
2010-04-19 21:08:53 -0700

    Adding "setup.py"

---
+++ django/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+      name = "mootools",
+      version = "0.2",
+      url = 'http://www.mootools.net',
+      description = "Depender: JS Dep loader",
+      install_requires = ['setuptools', 'django', 'PyYAML'],
+)
