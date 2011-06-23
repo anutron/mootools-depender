e8fd6fd01af3b052f03c3e3495550690c75ebbab
Aaron Newton <git@clientcide.com>
2011-06-22 18:08:53 -0700

    attempting fix for arian

--- django/setup.py
+++ django/setup.py
@@ -1,6 +1,28 @@
 from setuptools import setup, find_packages
 import os
 
+from setuptools.dist import Distribution
+import pkg_resources
+
+
+add_django_dependency = True
+# See issues #50, #57 and #58 for why this is necessary
+try:
+    pkg_resources.get_distribution('Django')
+    add_django_dependency = False
+except pkg_resources.DistributionNotFound:
+    try:
+        import django
+        if django.VERSION[0] >= 1 and django.VERSION[1] >= 1 and django.VERSION[2] >= 1:
+            add_django_dependency = False
+    except ImportError:
+        pass
+
+Distribution({
+    "setup_requires": add_django_dependency and  ['Django >=1.1.1'] or []
+})
+
+
 base = os.path.join(os.path.dirname(__file__), "src")
 
 setup(
